b7b6ac1e34dfb9b6e345bcfca97c7ab9e9edc9b9
Oshen_Dikkumbura <oshen@echonlabs.com>
2023-06-10 21:37:55 +0530

    changes

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./style.css";
+import { FaGithub, FaFacebookF, FaLinkedin, FaMedium } from "react-icons/fa";
+import { socialAccounts } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {socialAccounts.twitter && (
+          <li>
+            <a target="_blank" rel="noreferrer" href={socialAccounts.medium}>
+              <FaMedium />
+            </a>
+          </li>
+        )}
+        {socialAccounts.github && (
+          <li>
+            <a target="_blank" rel="noreferrer" href={socialAccounts.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialAccounts.facebook && (
+          <li>
+            <a target="_blank" rel="noreferrer" href={socialAccounts.facebook}>
+              <FaFacebookF />
+            </a>
+          </li>
+        )}
+        {socialAccounts.linkedin && (
+          <li>
+            <a target="_blank" rel="noreferrer" href={socialAccounts.linkedin}>
+              <FaLinkedin />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Follow Me</p>
+    </div>
+  );
+};
